c3f6cdab0c3de5da1bb7f1b8b9e1260a2183a36c
Drew Wilson <jankyy93@gmail.com>
2021-08-31 19:11:09 -0400

    Finished with explorer mode

--- src/functions.ts
+++ src/functions.ts
@@ -211,7 +211,9 @@ function someoneToLove(strings: string[]): boolean {
 type Hobbies = Record<string, { title: string; description: string }>
 function objectKeys(objectOfHobbies: Hobbies): string[] {
   // Replace the code below with your own code
-  return []
+  return Object.keys(objectOfHobbies).map(
+    object => `${object} - ${objectOfHobbies[object].title}`
+  )
 }
 
 /**
